b2fb07cb619908f6e762e05d5eebf763791d9558
thisishoon <jihoon522@naver.com>
2020-06-14 18:30:07 +0900

    update Model.py

--- DDiary2.py
+++ DDiary2.py
@@ -11,6 +11,7 @@ import numpy as np
 from keras_self_attention import SeqSelfAttention
 import json
 from konlpy.tag import Komoran
+import tensorflow as tf
 
 print("@@@@@@????")
 model = load_model('Project_model.h5',custom_objects={'SeqSelfAttention':SeqSelfAttention}, compile=False)
@@ -20,14 +21,10 @@ with open('wordIndex.json') as json_file:
     word_index = json.load(json_file)
     token.word_index = word_index
 
+
 def predict(paragraph):
     print("예측 시작!!!!!")
-    # mc = Komoran()
-    # model = load_model('Project_model.h5', custom_objects={'SeqSelfAttention': SeqSelfAttention})
-    # token = Tokenizer(9482)
-    # with open('wordIndex.json') as json_file:
-    #     word_index = json.load(json_file)
-    #     token.word_index = word_index
+
 
 
     emotions = [0,0,0,0,0,0]
--- Model.py
+++ Model.py
@@ -1,49 +1,53 @@
-﻿# coding=<utf-8>
-import pandas as pd
+﻿import pandas as pd
 import numpy as np
 import re
 from keras.preprocessing.sequence import pad_sequences
 
-def put(token, sentence, mc):  
-        sentence2 = []
-        sentence.replace('.','').replace(',','').replace('~','').replace('"','')
-        sentence2 = mc.morphs(sentence)
-        print(sentence2)
-        if len(sentence2)<2:
-            output= token.texts_to_sequences(sentence)
-        else:
-            output= token.texts_to_sequences(sentence2)
-        output = np.array(output)
-        return output
+
+def put(token, sentence, mc):
+    sentence2 = []
+    sentence.replace('.', '').replace(',', '').replace('~', '').replace('"', '')
+    sentence2 = mc.morphs(sentence)
+    print(sentence2)
+    if len(sentence2) < 2:
+        output = token.texts_to_sequences(sentence)
+    else:
+        output = token.texts_to_sequences(sentence2)
+    output = np.array(output)
+    return output
+
 
 # 결과 변환 함수
 def convert(input):
     result = np.argmax(input, axis=1)
     return result
 
-def out(sentence, model) :
-    #기본처리
-        final=np.array([])
 
-    #try:
-        k=0
-        print(sentence)
-        for nums in sentence:
-                if any(nums) == False:
-                        continue
-                final=np.append(final,nums)
-                k=k+1
-        if k == 0 :
-                return [5]
-        
-        final2=np.array([])
-        final2=np.append(final2,final)
-        zero = np.zeros((30-k, 1))
-        final2 = np.append(zero, final2)
-        final2=final2.reshape(30,1)
-        result = model.predict(final2.T)
-        print(result)
-        return convert(result)
+def out(sentence, model):
+    # 기본처리
+    final = np.array([])
 
-    #except:
-    #    return [5]
+    # try:
+    k = 0
+    print(sentence)
+    for nums in sentence:
+        if any(nums) == False:
+            continue
+        final = np.append(final, nums)
+        k = k + 1
+        if k == 29:
+            break
+    if k == 0:
+        return [5]
+
+    final2 = np.array([])
+    final2 = np.append(final2, final)
+    zero = np.zeros((30 - k, 1))
+    final2 = np.append(zero, final2)
+    final2 = final2.reshape(30, 1)
+    result = model.predict(final2.T)
+    print(result)
+    return convert(result)
+
+# except:
+#    return [5]
