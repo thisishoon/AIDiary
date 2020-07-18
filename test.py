debf3cad6783ca9204f318dca13c72aee59cc97e
thisishoon <jihoon522@naver.com>
2020-07-18 14:10:08 +0900

    make lightweight file size

--- test.py
+++
@@ -1,9 +0,0 @@
-﻿import DDiary
-
-diary = DDiary.ddiary() #모델 로드 및 형태소분석기 로드
-
-print("문단을 입력해보세요")
-para=input()
-print("슬픔, 중립, 행복, 불안, 분노, 예외 리스트입니다.")
-response = diary.predict(para)
-print(response)
